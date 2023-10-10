97a8fb23e51de1140eb0d5b856d3027d8b656826
Starciad <davigamerlsf16@gmail.com>
2023-10-10 18:49:42 -0300

    feat: refact: Revamp compilation and publication script.

--- src/Scripts/PowerShell/publish_all.ps1
+++ src/Scripts/PowerShell/publish_all.ps1
@@ -1,18 +1,28 @@
+# =================================== #
+#                                     #
+#   This script aims to assist in     #
+# compiling all projects and creating #
+# standardized folders in accordance  #
+#   with Stardust Defender version    #
+#       publishing regulations.       #
+#                                     #
+# =================================== #
+
 # Clear the console window
 Clear-Host
 
 # Define solutions and publishing directories
-$desktopGL = "..\StardustDefender\StardustDefender.DesktopGL.csproj"
-$windowsDX = "..\StardustDefender\StardustDefender.WindowsDX.csproj"
-$outputDirectory = "..\Publish"
+$desktopGL = "..\src\StardustDefender\StardustDefender.DesktopGL.csproj"
+$windowsDX = "..\src\StardustDefender\StardustDefender.WindowsDX.csproj"
+$outputDirectory = "..\src\Publish"
 
 # List of target platforms
 $platforms = @("win-x64", "linux-x64", "osx-x64")
 
 # Function to publish a project for a given platform
-function Publish-Project($projectPath, $platform) {
+function Publish-Project($projectName, $projectPath, $platform) {
     Write-Host "Publishing $projectPath for $platform..."
-    dotnet publish $projectPath -c Release -r $platform --output "$outputDirectory\stardust-defender-v-$platform"
+    dotnet publish $projectPath -c Release -r $platform --output "$outputDirectory\stardust-defender-$projectName-v0.0.0.0-$platform"
     Write-Host "Publishing to $platform completed."
 }
 
@@ -38,31 +48,22 @@ if (Test-Path $outputDirectory -PathType Container) {
 # Publish WindowsDX
 Clear-Host
 Write-Host "Publishing WindowsDX for Win-x64..."
-Publish-Project $windowsDX "win-x64"
-
-Start-Sleep -Milliseconds 2500
-Clear-Host
+Publish-Project "windowsdx" $windowsDX $platforms[0]
 
 # Publish DesktopGL for all platforms
 Write-Host "Publishing DesktopGL to all platforms..."
 foreach ($platform in $platforms) {
-    Publish-Project $desktopGL $platform
-    Start-Sleep -Milliseconds 1000
+    Publish-Project "desktopgl" $desktopGL $platform
     Write-Host "Next..."
-    Start-Sleep -Milliseconds 1500
-    Clear-Host
 }
 
 Write-Host "All publishing processes have been completed."
-Start-Sleep -Milliseconds 2500
-Clear-Host
 
 # Copy content folder and delete specific subdirectories
-Clear-Host
 Write-Host "Copying content folder..."
 
-$source = "..\StardustDefender\Content"
-$destination = "$outputDirectory\Content"
+$source = "..\src\StardustDefender\Content"
+$destination = "$outputDirectory\stardust-defender-v0.0.0.0-assets-full\Content"
 $subdirectoriesToDelete = @("bin", "obj")
 
 # Copy the source folder to the destination
@@ -72,6 +73,3 @@ Copy-Item -Path $source -Destination $destination -Recurse
 Delete-Subdirectories $destination $subdirectoriesToDelete
 
 Write-Host "Operation completed. The folder has been copied to $destination, and the specified subdirectories have been deleted."
-
-Start-Sleep -Milliseconds 2500
-Clear-Host
