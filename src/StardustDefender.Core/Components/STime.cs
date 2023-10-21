87e93c0f4c7ae1979989be155a68a7d977f5dd00
Starciad <davigamerlsf16@gmail.com>
2023-10-20 22:17:49 -0300

    feat: Add documentation in STime.cs.

--- src/StardustDefender.Core/Components/STime.cs
+++ src/StardustDefender.Core/Components/STime.cs
@@ -2,16 +2,34 @@
 
 namespace StardustDefender.Core.Components
 {
+    /// <summary>
+    /// Static utility class for managing game time information.
+    /// </summary>
     public static class STime
     {
+        /// <summary>
+        /// Gets the GameTime object for update time.
+        /// </summary>
         public static GameTime UpdateTime { get; private set; }
+
+        /// <summary>
+        /// Gets the GameTime object for draw time.
+        /// </summary>
         public static GameTime DrawTime { get; private set; }
 
+        /// <summary>
+        /// Sets the GameTime object for update time.
+        /// </summary>
+        /// <param name="value">The GameTime object for update time.</param>
         internal static void SetUpdateGameTime(GameTime value)
         {
             UpdateTime = value;
         }
 
+        /// <summary>
+        /// Sets the GameTime object for draw time.
+        /// </summary>
+        /// <param name="value">The GameTime object for draw time.</param>
         internal static void SetDrawGameTime(GameTime value)
         {
             DrawTime = value;
