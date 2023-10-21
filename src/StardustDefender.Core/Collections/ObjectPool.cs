72d8b5241db63e064dd52bc622a1b5844b501f7d
Starciad <davigamerlsf16@gmail.com>
2023-10-21 12:40:57 -0300

    feat: Add docs to ObjectPool collections.

--- src/StardustDefender.Core/Collections/ObjectPool.cs
+++ src/StardustDefender.Core/Collections/ObjectPool.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 
 namespace StardustDefender.Core.Collections
 {
-    internal sealed class ObjectPool<TObject> where TObject : IPoolableObject
+    /// <summary>
+    /// A generic object pool for managing reusable objects that implement the <see cref="IPoolableObject"/> interface.
+    /// </summary>
+    /// <typeparam name="TObject">The type of objects stored in the pool.</typeparam>
+    public sealed class ObjectPool<TObject> where TObject : IPoolableObject
     {
         private readonly Dictionary<Type, Queue<TObject>> _objectPool = new();
 
+        /// <summary>
+        /// Gets an object of type <typeparamref name="TKey"/> from the pool, if available.
+        /// </summary>
+        /// <typeparam name="TKey">The type of object to retrieve from the pool.</typeparam>
+        /// <returns>An object from the pool, or the default value of <typeparamref name="TObject"/> if the pool is empty.</returns>
         public TObject Get<TKey>() where TKey : TObject
         {
             return Get(typeof(TKey));
         }
+
+        /// <summary>
+        /// Gets an object of the specified type from the pool, if available.
+        /// </summary>
+        /// <param name="keyType">The type of object to retrieve from the pool.</param>
+        /// <returns>An object from the pool, or the default value of <typeparamref name="TObject"/> if the pool is empty.</returns>
         public TObject Get(Type keyType)
         {
             TObject target = default;
@@ -29,6 +44,10 @@ namespace StardustDefender.Core.Collections
             return target;
         }
 
+        /// <summary>
+        /// Returns an object to the pool for reuse.
+        /// </summary>
+        /// <param name="value">The object to return to the pool.</param>
         public void ReturnToPool(TObject value)
         {
             value.Reset();
