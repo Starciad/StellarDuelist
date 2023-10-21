df5d1cba6d38670e4d9504f1f833089305bc52bd
Starciad <davigamerlsf16@gmail.com>
2023-10-21 13:39:43 -0300

    feat: Add documentation in SDifficultyController.cs.

--- src/StardustDefender.Core/Controllers/Difficulty/SDifficultyController.Enemies.cs
+++ src/StardustDefender.Core/Controllers/Difficulty/SDifficultyController.Enemies.cs
@@ -15,11 +15,20 @@ namespace StardustDefender.Core.Controllers
     {
         private static readonly List<SEntityHeader> enemies = new();
 
+        /// <summary>
+        /// Creates a random enemy entity at the specified position.
+        /// </summary>
+        /// <param name="position">The position at which to create the enemy.</param>
+        /// <returns>The created enemy entity.</returns>
         internal static SEntity CreateRandomEnemy(Vector2 position)
         {
             return SEntityManager.Create(GetRandomEnemyType(), position);
         }
 
+        /// <summary>
+        /// Retrieves a random enemy type for spawning.
+        /// </summary>
+        /// <returns>The retrieved enemy type.</returns>
         private static Type GetRandomEnemyType()
         {
             return enemies.Where(x => x.CanSpawn).SelectRandom().EntityType;
