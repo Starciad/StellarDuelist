af3e5e8b2977fb85d578a4369902310f5545064c
Starciad <davigamerlsf16@gmail.com>
2023-10-24 18:32:02 -0300

    feat: Add documentation on entity construction components.

--- src/StardustDefender.Core/Entities/Templates/SBossEntity.cs
+++ src/StardustDefender.Core/Entities/Templates/SBossEntity.cs
@@ -4,6 +4,12 @@ using StardustDefender.Core.Controllers;
 
 namespace StardustDefender.Core.Entities.Templates
 {
+    /// <summary>
+    /// Base class template for creating bosses entities.
+    /// </summary>
+    /// <remarks>
+    /// With this template, a variety of functions, properties, and attributes are provided to automate certain processes and have references for internal work/configurations.
+    /// </remarks>
     public abstract class SBossEntity : SEntity
     {
         protected void CollideWithPlayer()
