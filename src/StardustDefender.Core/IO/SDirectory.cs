b3d4a5c74fff03cc701ff43a90b517c27e98c557
Starciad <davigamerlsf16@gmail.com>
2023-10-24 18:50:07 -0300

    feat: Add documentation on IO operation classes.

--- src/StardustDefender.Core/IO/SDirectory.cs
+++ src/StardustDefender.Core/IO/SDirectory.cs
@@ -3,6 +3,9 @@ using System.IO;
 
 namespace StardustDefender.Core.IO
 {
+    /// <summary>
+    /// Provides utility methods for working with directories in the game.
+    /// </summary>
     public static class SDirectory
     {
         private static string GAME_DIRECTORY => Directory.GetCurrentDirectory();
@@ -10,10 +13,16 @@ namespace StardustDefender.Core.IO
         {
             "Logs",
             "Settings",
+            "Screenshots",
+            "Gifs",
+            "Scores"
         };
 
         private static readonly Dictionary<string, string> directories = new();
 
+        /// <summary>
+        /// Initializes the default game directories if they don't exist.
+        /// </summary>
         public static void Initialize()
         {
             foreach (string directoryName in DEFAULT_DIRECTORIES)
@@ -27,6 +36,12 @@ namespace StardustDefender.Core.IO
                 directories.Add(directoryName, targetDirectoryPath);
             }
         }
+
+        /// <summary>
+        /// Gets the full path of a directory by its name.
+        /// </summary>
+        /// <param name="name">The name of the directory.</param>
+        /// <returns>The full path of the directory.</returns>
         public static string GetDirectoryPath(string name)
         {
             return directories[name];
