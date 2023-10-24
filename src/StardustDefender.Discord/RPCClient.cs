3d3e451cfdf2ad7e11a592ea96cf3db008fafc2c
Starciad <davigamerlsf16@gmail.com>
2023-10-24 19:44:45 -0300

    feat: Add documentation in RPCClient.cs.

--- src/StardustDefender.Discord/RPCClient.cs
+++ src/StardustDefender.Discord/RPCClient.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 
 namespace StardustDefender.Discord
 {
+    /// <summary>
+    /// RPC communication client with discord for synchronizing in-game information such as status on the player's profile.
+    /// </summary>
     public sealed class RPCClient
     {
         private DiscordRpcClient _client;
@@ -18,6 +21,9 @@ namespace StardustDefender.Discord
         private string details;
         private string state;
 
+        /// <summary>
+        /// Initializes the RPC client and its asynchronous updates.
+        /// </summary>
         public void Start()
         {
             this._client = new(Environment.GetEnvironmentVariable("DISCORD_RPC_CLIENT_ID"));
@@ -28,6 +34,10 @@ namespace StardustDefender.Discord
             _ = this._client.Initialize();
             _ = Task.Run(UpdateAsync);
         }
+
+        /// <summary>
+        /// Stop the RPC client.
+        /// </summary>
         public void Stop()
         {
             this._client.Dispose();
